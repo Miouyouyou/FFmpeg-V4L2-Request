12a0f4d8bd34deb5cb757e5847b340eb25b9a0dd
Miouyouyou (Myy) <myy@miouyouyou.fr>
2019-05-18 23:46:21 +0200

    Fixed the v4l2 check.
    We're checking for v4l2_request in particular.
    
    Signed-off-by: Miouyouyou (Myy) <myy@miouyouyou.fr>

--- compile.sh
+++ compile.sh
@@ -16,9 +16,10 @@ fi
 # Configure and die quickly if v4l2 is not enabled.
 # I don't understand why the configure script don't die by itself
 # when you request a feature that cannot be compiled...
-./configure --enable-v4l2-request --enable-libdrm --enable-libudev --enable-shared --enable-pic | grep "v4l2"
+./configure --enable-v4l2-request --enable-libdrm --enable-libudev --enable-shared --enable-pic | grep "v4l2_request"
 if [ ! $? = 0 ]; then
-	echo "No v4l2 request !?"
+	echo "Unable to enable v4l2 request support on FFmpeg"
+	echo "See ffmpeg/ffbuild/config.log"
 	exit
 fi
 
